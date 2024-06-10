c9721ac4199351f8d6bffd89f92ae5f03045d682
Olzhas Suleimen <amajisenpai@gmail.com>
2024-06-10 18:21:05 +0500

    Update 4.

--- test/http_100_continue_test.dart
+++ test/http_100_continue_test.dart
@@ -2,27 +2,29 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:async';
+import "dart:async";
 import 'dart:convert';
-import "dart:io" hide HttpServer, HttpClient;
+import "dart:typed_data";
 
 import "package:http_io/http_io.dart";
 import "package:test/test.dart";
 
 Future<Null> doTest(responseBytes, bodyLength) async {
-  fullRequest(bytes) {
-    var len = bytes.length;
-    return len > 4 &&
-        bytes[len - 4] == 13 &&
-        bytes[len - 3] == 10 &&
-        bytes[len - 2] == 13 &&
-        bytes[len - 1] == 10;
+  bool fullRequest(List<int> bytes) {
+    int length = bytes.length;
+    return length > 4 &&
+        bytes[length - 4] == 13 &&
+        bytes[length - 3] == 10 &&
+        bytes[length - 2] == 13 &&
+        bytes[length - 1] == 10;
   }
 
-  handleSocket(socket) async {
-    var bytes = [];
-    await for (var data in socket) {
+  Future<void> handleSocket(Socket socket) async {
+    List<int> bytes = <int>[];
+
+    await for (Uint8List data in socket) {
       bytes.addAll(data);
+
       if (fullRequest(bytes)) {
         socket.add(responseBytes);
         socket.close();
@@ -30,28 +32,30 @@ Future<Null> doTest(responseBytes, bodyLength) async {
     }
   }
 
-  var server = await ServerSocket.bind('127.0.0.1', 0);
+  ServerSocket server = await ServerSocket.bind('127.0.0.1', 0);
   server.listen(handleSocket);
 
-  var client = HttpClient();
-  var request =
-      await client.getUrl(Uri.parse('http://127.0.0.1:${server.port}/'));
-  var response = await request.close();
+  HttpClient client = HttpClient();
+  Uri url = Uri.parse('http://127.0.0.1:${server.port}/');
+  HttpClientRequest request = await client.getUrl(url);
+  HttpClientResponse response = await request.close();
   expect(response.statusCode, equals(200));
-  expect(bodyLength,
-      equals((await response.fold([], (p, e) => p..addAll(e))).length));
+
+  List<int> bytes = <int>[];
+  await response.forEach(bytes.addAll);
+  expect(bodyLength, equals(bytes.length));
   await server.close();
 }
 
-main() {
-  var r1 = '''
+void main() {
+  String r1 = '''
 HTTP/1.1 100 Continue\r
 \r
 HTTP/1.1 200 OK\r
 \r
 ''';
 
-  var r2 = '''
+  String r2 = '''
 HTTP/1.1 100 Continue\r
 My-Header-1: hello\r
 My-Header-2: world\r
@@ -60,7 +64,7 @@ HTTP/1.1 200 OK\r
 \r
 ''';
 
-  var r3 = '''
+  String r3 = '''
 HTTP/1.1 100 Continue\r
 \r
 HTTP/1.1 200 OK\r
@@ -68,13 +72,31 @@ Content-Length: 2\r
 \r
 AB''';
 
-  test("Continue OK", () async {
-    await doTest(ascii.encode(r1), 0);
+  group('CRLF', () {
+    test("Continue OK", () async {
+      await doTest(ascii.encode(r1), 0);
+    });
+
+    test("Continue hello world OK", () async {
+      await doTest(ascii.encode(r2), 0);
+    });
+
+    test("Continue OK length AB", () async {
+      await doTest(ascii.encode(r3), 2);
+    });
   });
-  test("Continue hello world OK", () async {
-    await doTest(ascii.encode(r2), 0);
-  });
-  test("Continue OK length AB", () async {
-    await doTest(ascii.encode(r3), 2);
+
+  group('LF', () {
+    test("Continue OK", () async {
+      await doTest(ascii.encode(r1.replaceAll('\r\n', '\n')), 0);
+    });
+
+    test("Continue hello world OK", () async {
+      await doTest(ascii.encode(r2.replaceAll('\r\n', '\n')), 0);
+    });
+
+    test("Continue OK length AB", () async {
+      await doTest(ascii.encode(r3.replaceAll('\r\n', '\n')), 2);
+    });
   });
 }
--- test/http_10_test.dart
+++ test/http_10_test.dart
@@ -2,8 +2,11 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:async';
-import "dart:io" hide HttpServer, HttpClient, HttpRequest, HttpException;
+// ignore_for_file: avoid_print
+
+import "dart:async";
+import "dart:isolate";
+import "dart:typed_data";
 
 import "package:http_io/http_io.dart";
 import "package:test/test.dart";
@@ -11,218 +14,251 @@ import "package:test/test.dart";
 // Client makes a HTTP 1.0 request without connection keep alive. The
 // server sets a content length but still needs to close the
 // connection as there is no keep alive.
-Future<Null> testHttp10NoKeepAlive() {
-  Completer<Null> completer = Completer();
-  HttpServer.bind("127.0.0.1", 0).then((server) {
-    server.listen((HttpRequest request) {
-      expect(request.headers.value('content-length'), isNull);
-      expect(-1, equals(request.contentLength));
-      var response = request.response;
-      response.contentLength = 1;
-      expect("1.0", equals(request.protocolVersion));
-      response.done
-          .then((_) => fail("Unexpected response completion"))
-          .catchError((error) => expect((error is HttpException), isTrue));
-      response.write("Z");
-      response.write("Z");
-      response.close();
-    }, onError: (e, trace) {
-      String msg = "Unexpected error $e";
-      if (trace != null) msg += "\nStackTrace: $trace";
-      fail(msg);
+Future<void> testHttp10NoKeepAlive() async {
+  Completer<void> completer = Completer<void>();
+
+  HttpServer server = await HttpServer.bind("127.0.0.1", 0);
+
+  server.listen((HttpRequest request) {
+    expect(request.headers.value('content-length'), isNull);
+    expect(-1, equals(request.contentLength));
+
+    HttpResponse response = request.response;
+    response.contentLength = 1;
+    expect("1.0", equals(request.protocolVersion));
+
+    Future(() async {
+      try {
+        await response.done;
+        fail("Unexpected response completion");
+      } catch (error) {
+        expect(error, isA<HttpException>());
+      }
     });
 
-    int count = 0;
-    makeRequest() {
-      Socket.connect("127.0.0.1", server.port).then((socket) {
-        socket.write("GET / HTTP/1.0\r\n\r\n");
+    response.write("Z");
+    response.write("Z");
+    response.close();
+    expect(() => response.write("x"), throwsA(isA<StateError>()));
+  }, onError: (error, stackTrace) {
+    String message = "Unexpected error $error";
 
-        List<int> response = [];
-        socket.listen(response.addAll, onDone: () {
-          count++;
-          socket.destroy();
-          String s = String.fromCharCodes(response).toLowerCase();
-          expect(-1, equals(s.indexOf("keep-alive")));
-          if (count < 10) {
-            makeRequest();
-          } else {
-            server.close();
-            completer.complete(null);
-          }
-        });
-      });
+    if (stackTrace != null) {
+      message += "\nStackTrace: $stackTrace";
     }
 
-    makeRequest();
+    fail(message);
   });
-  return completer.future;
+
+  int count = 0;
+
+  Future<void> makeRequest() async {
+    Socket socket = await Socket.connect("127.0.0.1", server.port);
+    socket.write("GET / HTTP/1.0\r\n\r\n");
+
+    List<int> response = <int>[];
+
+    socket.listen(response.addAll, onDone: () {
+      count++;
+      socket.destroy();
+
+      String value = String.fromCharCodes(response).toLowerCase();
+      expect(-1, equals(value.indexOf("keep-alive")));
+
+      if (count < 10) {
+        makeRequest();
+      } else {
+        server.close();
+        completer.complete();
+      }
+    });
+  }
+
+  makeRequest();
+  await completer.future;
 }
 
 // Client makes a HTTP 1.0 request and the server does not set a
 // content length so it has to close the connection to mark the end of
 // the response.
-Future<Null> testHttp10ServerClose() {
-  Completer<Null> completer = Completer();
-  HttpServer.bind("127.0.0.1", 0).then((server) {
-    server.listen((HttpRequest request) {
-      expect(request.headers.value('content-length'), isNull);
-      expect(-1, equals(request.contentLength));
-      request.listen((_) {}, onDone: () {
-        var response = request.response;
-        expect("1.0", equals(request.protocolVersion));
-        response.write("Z");
-        response.close();
-      });
-    }, onError: (e, trace) {
-      String msg = "Unexpected error $e";
-      if (trace != null) msg += "\nStackTrace: $trace";
-      fail(msg);
+Future<void> testHttp10ServerClose() async {
+  Completer<void> completer = Completer<void>();
+
+  HttpServer server = await HttpServer.bind("127.0.0.1", 0);
+
+  server.listen((HttpRequest request) {
+    expect(request.headers.value('content-length'), isNull);
+    expect(-1, equals(request.contentLength));
+    request.listen(null, onDone: () {
+      HttpResponse response = request.response;
+      expect("1.0", equals(request.protocolVersion));
+      response.write("Z");
+      response.close();
     });
+  }, onError: (error, stackTrace) {
+    String message = "Unexpected error $error";
 
-    int count = 0;
-    makeRequest() {
-      Socket.connect("127.0.0.1", server.port).then((socket) {
-        socket.write("GET / HTTP/1.0\r\n");
-        socket.write("Connection: Keep-Alive\r\n\r\n");
-
-        List<int> response = [];
-        socket.listen(response.addAll,
-            onDone: () {
-              socket.destroy();
-              count++;
-              String s = String.fromCharCodes(response).toLowerCase();
-              expect("z", equals(s[s.length - 1]));
-              expect(-1, equals(s.indexOf("content-length:")));
-              expect(-1, equals(s.indexOf("keep-alive")));
-              if (count < 10) {
-                makeRequest();
-              } else {
-                server.close();
-                completer.complete(null);
-              }
-            },
-            onError: (e) => print(e));
-      });
+    if (stackTrace != null) {
+      message += "\nStackTrace: $stackTrace";
     }
 
-    makeRequest();
+    fail(message);
   });
-  return completer.future;
+
+  int count = 0;
+
+  Future<void> makeRequest() async {
+    Socket socket = await Socket.connect("127.0.0.1", server.port);
+    socket.write("GET / HTTP/1.0\r\n");
+    socket.write("Connection: Keep-Alive\r\n\r\n");
+
+    List<int> response = <int>[];
+    socket.listen(response.addAll, onDone: () {
+      socket.destroy();
+      count++;
+
+      String value = String.fromCharCodes(response).toLowerCase();
+      expect("z", equals(value[value.length - 1]));
+      expect(-1, equals(value.indexOf("content-length:")));
+      expect(-1, equals(value.indexOf("keep-alive")));
+
+      if (count < 10) {
+        makeRequest();
+      } else {
+        server.close();
+        completer.complete();
+      }
+    }, onError: print);
+  }
+
+  makeRequest();
+  await completer.future;
 }
 
 // Client makes a HTTP 1.0 request with connection keep alive. The
 // server sets a content length so the persistent connection can be
 // used.
-Future<Null> testHttp10KeepAlive() {
-  Completer<Null> completer = Completer();
-  HttpServer.bind("127.0.0.1", 0).then((server) {
-    server.listen((HttpRequest request) {
-      expect(request.headers.value('content-length'), isNull);
-      expect(-1, equals(request.contentLength));
-      var response = request.response;
-      response.contentLength = 1;
-      response.persistentConnection = true;
-      expect("1.0", equals(request.protocolVersion));
-      response.write("Z");
-      response.close();
-    }, onError: (e, trace) {
-      String msg = "Unexpected error $e";
-      if (trace != null) msg += "\nStackTrace: $trace";
-      fail(msg);
-    });
+Future<void> testHttp10KeepAlive() async {
+  Completer<void> completer = Completer<void>();
 
-    Socket.connect("127.0.0.1", server.port).then((socket) {
-      void sendRequest() {
-        socket.write("GET / HTTP/1.0\r\n");
-        socket.write("Connection: Keep-Alive\r\n\r\n");
-      }
+  HttpServer server = await HttpServer.bind("127.0.0.1", 0);
 
-      List<int> response = [];
-      int count = 0;
-      socket.listen((d) {
-        response.addAll(d);
-        if (response[response.length - 1] == "Z".codeUnitAt(0)) {
-          String s = String.fromCharCodes(response).toLowerCase();
-          expect(s.indexOf("\r\nconnection: keep-alive\r\n") > 0, isTrue);
-          expect(s.indexOf("\r\ncontent-length: 1\r\n") > 0, isTrue);
-          count++;
-          if (count < 10) {
-            response = [];
-            sendRequest();
-          } else {
-            socket.close();
-          }
-        }
-      }, onDone: () {
-        socket.destroy();
-        server.close();
-        completer.complete(null);
-      });
-      sendRequest();
-    });
+  server.listen((HttpRequest request) {
+    expect(request.headers.value('content-length'), isNull);
+    expect(-1, equals(request.contentLength));
+
+    HttpResponse response = request.response;
+    response.contentLength = 1;
+    response.persistentConnection = true;
+    expect("1.0", equals(request.protocolVersion));
+    response.write("Z");
+    response.close();
+  }, onError: (error, stackTrace) {
+    String message = 'Unexpected error $error';
+
+    if (stackTrace != null) {
+      message += '\nStackTrace: $stackTrace';
+    }
+
+    fail(message);
   });
-  return completer.future;
+
+  Socket socket = await Socket.connect("127.0.0.1", server.port);
+
+  void sendRequest() {
+    socket.write("GET / HTTP/1.0\r\n");
+    socket.write("Connection: Keep-Alive\r\n\r\n");
+  }
+
+  List<int> response = <int>[];
+  int count = 0;
+
+  socket.listen((Uint8List data) {
+    response.addAll(data);
+
+    if (response[response.length - 1] == "Z".codeUnitAt(0)) {
+      String value = String.fromCharCodes(response).toLowerCase();
+      expect(value.indexOf("\r\nconnection: keep-alive\r\n") > 0, isTrue);
+      expect(value.indexOf("\r\ncontent-length: 1\r\n") > 0, isTrue);
+      count++;
+
+      if (count < 10) {
+        response = <int>[];
+        sendRequest();
+      } else {
+        socket.close();
+      }
+    }
+  }, onDone: () {
+    socket.destroy();
+    server.close();
+    completer.complete();
+  });
+
+  sendRequest();
+  await completer.future;
 }
 
 // Client makes a HTTP 1.0 request with connection keep alive. The
 // server does not set a content length so it cannot honor connection
 // keep alive.
-Future<Null> testHttp10KeepAliveServerCloses() {
-  Completer<Null> completer = Completer();
-  HttpServer.bind("127.0.0.1", 0).then((server) {
-    server.listen((HttpRequest request) {
-      expect(request.headers.value('content-length'), isNull);
-      expect(-1, equals(request.contentLength));
-      var response = request.response;
-      expect("1.0", equals(request.protocolVersion));
-      response.write("Z");
-      response.close();
-    }, onError: (e, trace) {
-      String msg = "Unexpected error $e";
-      if (trace != null) msg += "\nStackTrace: $trace";
-      fail(msg);
-    });
+Future<void> testHttp10KeepAliveServerCloses() async {
+  Completer<void> completer = Completer();
 
-    int count = 0;
-    makeRequest() {
-      Socket.connect("127.0.0.1", server.port).then((socket) {
-        socket.write("GET / HTTP/1.0\r\n");
-        socket.write("Connection: Keep-Alive\r\n\r\n");
+  HttpServer server = await HttpServer.bind("127.0.0.1", 0);
 
-        List<int> response = [];
-        socket.listen(response.addAll, onDone: () {
-          socket.destroy();
-          count++;
-          String s = String.fromCharCodes(response).toLowerCase();
-          expect("z", equals(s[s.length - 1]));
-          expect(-1, equals(s.indexOf("content-length")));
-          expect(-1, equals(s.indexOf("connection")));
-          if (count < 10) {
-            makeRequest();
-          } else {
-            server.close();
-            completer.complete(null);
-          }
-        });
-      });
+  server.listen((HttpRequest request) {
+    expect(request.headers.value('content-length'), isNull);
+    expect(-1, equals(request.contentLength));
+
+    HttpResponse response = request.response;
+    expect("1.0", equals(request.protocolVersion));
+    response.write("Z");
+    response.close();
+  }, onError: (error, stackTrace) {
+    String message = "Unexpected error $error";
+
+    if (stackTrace != null) {
+      message += "\nStackTrace: $stackTrace";
     }
 
-    makeRequest();
+    fail(message);
   });
-  return completer.future;
+
+  int count = 0;
+
+  Future<void> makeRequest() async {
+    Socket socket = await Socket.connect("127.0.0.1", server.port);
+    socket.write("GET / HTTP/1.0\r\n");
+    socket.write("Connection: Keep-Alive\r\n\r\n");
+
+    List<int> response = <int>[];
+
+    socket.listen(response.addAll, onDone: () {
+      socket.destroy();
+      count++;
+
+      String value = String.fromCharCodes(response).toLowerCase();
+      expect("z", equals(value[value.length - 1]));
+      expect(-1, equals(value.indexOf("content-length")));
+      expect(-1, equals(value.indexOf("connection")));
+
+      if (count < 10) {
+        makeRequest();
+      } else {
+        server.close();
+        completer.complete();
+      }
+    });
+  }
+
+  makeRequest();
+  await completer.future;
 }
 
 void main() {
-  test("Http10NoKeepAlive", () async {
-    await testHttp10NoKeepAlive();
-  });
-  test("Http10ServerClose", () async {
-    await testHttp10ServerClose();
-  });
-  test("Http10KeepAlive", () async {
-    await testHttp10KeepAlive();
-  });
-  test("Http10KeepAliveServerCloses", () async {
-    await testHttp10KeepAliveServerCloses();
-  });
+  test("Http10NoKeepAlive", testHttp10NoKeepAlive);
+  test("Http10ServerClose", testHttp10ServerClose);
+  test("Http10KeepAlive", testHttp10KeepAlive);
+  test("Http10KeepAliveServerCloses", testHttp10KeepAliveServerCloses);
 }
